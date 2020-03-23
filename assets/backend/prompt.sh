53dcb1a23696c73695ee6543069d01550d44fc65
Denis Kramer <d.kramer@soton.ac.uk>
2020-03-23 14:55:42 +0100

    Removed hostmgr dependence and based host authentication on signed certificates

--- assets/backend/prompt.sh
+++ assets/backend/prompt.sh
@@ -1,5 +1,5 @@
 # get current branch in git repo
-function parse_git_branch() {
+function parse_git_branch {
 	BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
 	if [ ! "${BRANCH}" == "" ]
 	then
